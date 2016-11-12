0fab38556501f2877fba52d2df83c0b34d37258f
zecheeseking <christopher.dale.@student.howest.be>
2016-11-12 16:48:19 +0100

    Implemented master GA GameObject and general flow of GA.

--- DarwinsWalkers/Assets/Scripts/GA/GeneticAlgorithm.cs
+++ DarwinsWalkers/Assets/Scripts/GA/GeneticAlgorithm.cs
@@ -15,11 +15,12 @@ public class GeneticAlgorithm{
     private float _mutationRate = 0.1f;
     private int _currentGeneration = 0;
     public int Generation { get { return _currentGeneration; } }
-    private delegate Individual PopulationSelection(List<Individual> pop);
+    private delegate Individual[] PopulationSelection(List<Individual> pop);
     private PopulationSelection _selection;
 
-    public GeneticAlgorithm(List<Individual> _population, SelectionType selectionType)
+    public GeneticAlgorithm(List<Individual> population, SelectionType selectionType)
     {
+        _population = population;
 
         _selection = AssignSelection(selectionType);
     }
@@ -27,6 +28,8 @@ public class GeneticAlgorithm{
     public void Evolve()
     {
         Debug.Log("To do: Evolve stuff");
+
+        var parents = _selection(_population);
     }
 
     private PopulationSelection AssignSelection(SelectionType selectionType)
@@ -40,7 +43,33 @@ public class GeneticAlgorithm{
     }
 
 
-    private Individual RouletteWheelSelection(List<Individual> pop)
+    private Individual[] RouletteWheelSelection(List<Individual> pop)
+    {
+        float totalFitness = TotalPopulationFitness(pop);
+        Individual[] parents = new Individual[2];
+
+        for(int i = 0; i < parents.Length; ++i)
+        {
+            float random = Random.Range(0, totalFitness);
+            float s = 0.0f;
+
+            foreach(Individual ind in  pop)
+            {
+                s += ind.Fitness;
+
+                if (s > random)
+                {
+                    parents[i] = ind;
+                    break;
+                }
+            }
+        }
+
+        return parents;
+    }
+
+
+    private Individual[] BreedingPoolSelection(List<Individual> pop)
     {
         return null;
     }
@@ -53,10 +82,5 @@ public class GeneticAlgorithm{
             f += i.Fitness;
 
         return f;
-    }
-
-    private Individual BreedingPoolSelection(List<Individual> pop)
-    {
-        return null;
     }
 }
---
+++ DarwinsWalkers/Assets/Scripts/GATestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GATestScript : MonoBehaviour {
+
+    GeneticAlgorithm _ga;
+
+	// Use this for initialization
+	void Awake () {
+        //Initialize starting population Randomly
+        //Initialize GA
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
