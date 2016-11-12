2393d8efb261590d6edc9804ebea3d7095a03052
zecheeseking <christopher.dale.@student.howest.be>
2016-11-12 16:29:10 +0100

    Implemented initial structure of GA

---
+++ DarwinsWalkers/Assets/Scripts/GA/GeneticAlgorithm.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GeneticAlgorithm{
+
+    public enum SelectionType
+    {
+        Roulette,
+        BreedingPool
+    }
+
+    private List<Individual> _population;
+
+    private float _mutationRate = 0.1f;
+    private int _currentGeneration = 0;
+    public int Generation { get { return _currentGeneration; } }
+    private delegate Individual PopulationSelection(List<Individual> pop);
+    private PopulationSelection _selection;
+
+    public GeneticAlgorithm(List<Individual> _population, SelectionType selectionType)
+    {
+
+        _selection = AssignSelection(selectionType);
+    }
+
+    public void Evolve()
+    {
+        Debug.Log("To do: Evolve stuff");
+    }
+
+    private PopulationSelection AssignSelection(SelectionType selectionType)
+    {
+        if (selectionType == SelectionType.Roulette)
+            return RouletteWheelSelection;
+        else if (selectionType == SelectionType.BreedingPool)
+            return BreedingPoolSelection;
+
+        return null;
+    }
+
+
+    private Individual RouletteWheelSelection(List<Individual> pop)
+    {
+        return null;
+    }
+
+    private float TotalPopulationFitness(List<Individual> pop)
+    {
+        float f = 0.0f;
+
+        foreach (Individual i in pop)
+            f += i.Fitness;
+
+        return f;
+    }
+
+    private Individual BreedingPoolSelection(List<Individual> pop)
+    {
+        return null;
+    }
+}
---
+++ DarwinsWalkers/Assets/Scripts/GA/Individual.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Individual {
+
+    public string _gene;
+    public GameObject _organism;
+    public int _fitness;
+
+    abstract public void SetGene();
+    abstract public float Fitness { get; set; }
+    abstract public void UpdateAttributesFromGene();
+    abstract public void Mutate();
+}
